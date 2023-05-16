704746c5cf9d395c0647b9fd4dd22760fe65d696
Niko Shin <nshin17@gmail.com>
2023-05-16 13:59:59 -0700

    Added build directory for firebase

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+
+### Rules:
+  # User clicks on an area in the photo which enables a 'targeting box' around click.
+  # Target box contains a list of possible characters.
+  # User selects one of the characters which enables a check with backend server to see if target box contains the character.
+  # Provide feedback to user => If correct, place a marker on character else an error message.
+  # Remove targeting box after message in both cases.
+  # Provide a timer from photo first load to successfully finding last character.
+  # After round is completed, get input of player and time to be recorded. Anonymous users can be added.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { initializeApp } from "firebase/app";
 import App from "./App";
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCT3Y_Iajiwr-OyORkTWDnPs4ghJkW3Wik",
+  authDomain: "top-where-is-waldo.firebaseapp.com",
+  projectId: "top-where-is-waldo",
+  storageBucket: "top-where-is-waldo.appspot.com",
+  messagingSenderId: "911739043380",
+  appId: "1:911739043380:web:694e3bc92cf298a5b65461",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
