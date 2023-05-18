999f4b4c32cbed05319e75b3ddea922faeaf5604
Niko Shin <nshin17@gmail.com>
2023-05-17 18:46:57 -0700

    Added timer interval and timer display.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,10 @@
+import React, { useEffect, useState } from "react";
 import CharacterImage from "./CharacterImage";
 import GameImage from "./GameImage";
 
-function Main({ allCharacters }) {
+function Main({ allCharacters, gameInPlay }) {
+  const [seconds, setSeconds] = useState(0);
+
   const clickedCoordinates = (event) => {
     const imageWidth = event.target.offsetWidth;
     const imageHeight = event.target.offsetHeight;
@@ -11,8 +14,35 @@ function Main({ allCharacters }) {
     return { xPercentage, yPercentage };
   };
 
+  useEffect(() => {
+    let interval = null;
+    if (gameInPlay) {
+      interval = setInterval(() => {
+        setSeconds((prev) => prev + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [gameInPlay]);
+
+  const renderTimer = () => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const displayTime = `${minutes
+      .toString()
+      .padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
+
+    return gameInPlay ? (
+      <div className="render_timer">{displayTime}</div>
+    ) : (
+      "0:00"
+    );
+  };
+
   return (
     <div className="main--container">
+      {renderTimer()}
       <CharacterImage allCharacters={allCharacters} />
       <GameImage handleClick={clickedCoordinates} />
     </div>
