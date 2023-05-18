51eb5baa6e2edb47917110f52e359b95915db1c3
Niko Shin <nshin17@gmail.com>
2023-05-17 18:46:11 -0700

    Game timer added.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 import characterData from "./characterData";
 import Main from "./components/Main";
 import "./styles.css";
+import LandingPage from "./components/LandingPage";
 
 function App() {
-  // const [gameInPlay, setGameInPlay] = useState(false);
+  const [gameInPlay, setGameInPlay] = useState(false);
   // const [charactersFound, setCharactersFound] = useState({});
   const [allCharacters, setAllCharacters] = useState(characterData);
 
+  const gamePlayTrue = () => {
+    setGameInPlay(true);
+  };
+
   return (
     <div className="app_container">
-      <Main allCharacters={allCharacters} />
+      <LandingPage gameIsInPlay={gamePlayTrue} />
+      <Main allCharacters={allCharacters} gameInPlay={gameInPlay} />
     </div>
   );
 }
